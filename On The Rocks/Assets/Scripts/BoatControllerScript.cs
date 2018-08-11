5878bb4112f0879c5c52bbd0e82f7f0ebcd6d169
Robert D <robert.drsj@gmail.com>
2018-08-11 18:46:28 -0500

    v0.0.2.2 - Physics Overhaul
    Cleaned up scripts and better utilized in-engine physics. Fixed collider and stacking bugs.

--- On The Rocks/Assets/Scripts/BoatControllerScript.cs
+++ On The Rocks/Assets/Scripts/BoatControllerScript.cs
@@ -2,27 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof (FloatObjectScript))]
 public class BoatControllerScript : MonoBehaviour {
 
-    public Vector2 COM;                             // Center Of Mass
+    public Transform m_COM;                         // center of mass child object
     [Space(15)]
-    public float speed = 1.0f;
-    public float movementThreshold = 25.0f;
-    [Space(15)]
-    public float rotationRate = 1.0f;
-    public float rotationThreshold = 25.0f;
-    public float maxRotation = 15.0f;
+    public float thrust = 1.0f;
 
     Rigidbody2D rb2d;
-    Transform m_COM;
     float horizontalInput;
-    float movementFactor;
-
-    float zRotation;
-    float rotationSign;
-    float rotationFactor;
-
     bool inputFlag;
 
 
@@ -36,7 +23,6 @@ public class BoatControllerScript : MonoBehaviour {
         //InputFlagging();
         Balance();
         Movement();
-        //Tilt();
 	}
 
     void InputFlagging()
@@ -49,27 +35,12 @@ public class BoatControllerScript : MonoBehaviour {
 
     void Balance()
     {
-        if (!m_COM)
-        {
-            m_COM = new GameObject("COM").transform;
-            m_COM.SetParent(transform);
-        }
-
-        m_COM.position = COM;
         GetComponent<Rigidbody2D>().centerOfMass = m_COM.position;
     }
 
     void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        movementFactor = Mathf.Lerp(movementFactor, horizontalInput, Time.deltaTime / movementThreshold);
-        transform.Translate(movementFactor * speed, 0.0f, 0.0f);
-    }
-
-    void Tilt()
-    {
-        rotationSign = Mathf.Sign(horizontalInput);
-        rotationFactor = Mathf.Lerp(rotationFactor, horizontalInput, Time.deltaTime / rotationThreshold);
-        transform.Rotate(0.0f, 0.0f, rotationFactor * rotationRate);
+        rb2d.AddForce(transform.right * horizontalInput * thrust);
     }
 }
